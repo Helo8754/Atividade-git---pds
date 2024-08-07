6c30b4226db9f551121732d45e2e3cf128bb0aab
Helo8754 <heloyse.maria@escolar.ifrn.edu.br>
2024-08-07 16:57:43 -0300

    concessionária

--- concessionária/urls.py
+++ concessionária/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from loja.views import Heloyse
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('Heloyse', Heloyse, nome= "Heloyse")
 ]
